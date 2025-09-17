9827766c057201e34e7f26b36cc22360e40b625d
Dmitrii Schilling <schiling2000@gmail.com>
2025-09-17 22:07:24 +0200

    Added two functions: one to display desserts and one for main dishes, and a cart that remains always visible.

--- db.js
+++ db.js
@@ -1,55 +1,53 @@
 let restaurant = 
     {
-        'name': 'La Bella Vita',
-        'description': 'Authentische italienische Küche mit klassischen Pasta-, Pizza- und Dessert-Spezialitäten.',
-        'rating': 4.3,
         'dishes': [
             {
-                'name_of_dishes': 'Spaghetti Carbonara',
+                'name_of_dish': 'Spaghetti Carbonara',
                 'ingredients': 'Spaghetti mit cremiger Sauce aus Ei, Parmesan, Pancetta und schwarzem Pfeffer.',
                 'price': 12.50
             },
             {
-                'name_of_dishes': 'Lasagne al Forno',
+                'name_of_dish': 'Lasagne al Forno',
                 'ingredients': 'Schichten aus Nudelplatten, Hackfleisch, Tomatensauce und Käse, im Ofen gebacken.',
                 'price': 13.90
             },
             {
-                'name_of_dishes': 'Risotto ai Funghi',
+                'name_of_dish': 'Risotto ai Funghi',
                 'ingredients': 'Cremiger Risotto mit frischen Waldpilzen, Parmesan und einem Hauch Weißwein.',
                 'price': 14.20
             },
             {
-                'name_of_dishes': 'Pizza Margherita',
+                'name_of_dish': 'Pizza Margherita',
                 'ingredients': 'Dünner Pizzateig mit Tomatensauce, Mozzarella und frischem Basilikum.',
                 'price': 9.80
             },
             {
-                'name_of_dishes': 'Saltimbocca alla Romana',
+                'name_of_dish': 'Saltimbocca alla Romana',
                 'ingredients': 'Kalbfleisch mit Schinken und Salbei in Weißweinsauce, serviert mit Gemüse oder Kartoffeln.',
                 'price': 18.50
             }
         ],
         'desserts': [
             {
-                'name_of_desserts': 'Panna Cotta',
+                'name_of_dessert': 'Panna Cotta',
                 'ingredients': 'Cremiger Vanillepudding mit Beerensauce.',
                 'price': 5.90
             },
             {
-                'name_of_desserts': 'Cannoli',
+                'name_of_dessert': 'Cannoli',
                 'ingredients': 'Knusprige Teigröllchen gefüllt mit süßer Ricotta-Creme.',
                 'price': 4.80
             },
             {
-                'name_of_desserts': 'Gelato',
+                'name_of_dessert': 'Gelato',
                 'ingredients': 'Italienisches Eis in verschiedenen Geschmacksrichtungen.',
                 'price': 3.50
             },
             {
-                'name_of_desserts': 'Tiramisu',
+                'name_of_dessert': 'Tiramisu',
                 'ingredients': 'Dessert aus Löffelbiskuits, Kaffee, Mascarpone-Creme und Kakaopulver.',
                 'price': 6.50
             }
         ]
     };
+
--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-function basketToggle(){
+function basketToggle() {
     let basketRef = document.getElementById('basket');
     console.log(basketRef);
     basketRef.classList.toggle('d_none');
 }
 
+function dihesRender() {
+    let dihesContainerRef = document.getElementById('dishes_container')
+    for (let index = 0; index < restaurant.dishes.length; index++) {
+        dihesContainerRef.innerHTML += `<div id="dish_container_${index}" class="dish_container">
+        <h4>${restaurant.dishes[index].name_of_dish}</h4>
+        <span class="description_of_dish">${restaurant.dishes[index].ingredients}</span>
+        <span class="price">${restaurant.dishes[index].price}€</span>
+        <svg class="plus_symbol" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+            <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 
+            0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+          </svg>
+          
+</div>`
+
+    }
+}
+
+function dessertsRender() {
+    let dihesContainerRef = document.getElementById('desserts_container')
+    for (let index = 0; index < restaurant.desserts.length; index++) {
+        dihesContainerRef.innerHTML += `<div id="dish_container_${index}" class="dish_container">
+    <h4>${restaurant.desserts[index].name_of_dessert}</h4>
+    <span class="description_of_dish">${restaurant.desserts[index].ingredients}</span>
+    <span class="price">${restaurant.desserts[index].price}€</span>
+    <svg class="plus_symbol" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+        <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 
+        0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+      </svg>
+      
+</div>`
+
+    }
+}
+
+function init() {
+    dihesRender()
+    dessertsRender()
+}
