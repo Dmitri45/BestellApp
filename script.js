4638408d4ff5186b1b2500bfb5718e92c65a2fef
Dmitrii Schilling <schiling2000@gmail.com>
2025-09-18 20:21:31 +0200

    git commit -m "Add functionality to add selected items to the cart"

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let basket = [];
+
 function basketToggle() {
     let basketRef = document.getElementById('basket');
     console.log(basketRef);
@@ -7,38 +9,86 @@ function basketToggle() {
 function dihesRender() {
     let dihesContainerRef = document.getElementById('dishes_container')
     for (let index = 0; index < restaurant.dishes.length; index++) {
-        dihesContainerRef.innerHTML += `<div id="dish_container_${index}" class="dish_container">
-        <h4>${restaurant.dishes[index].name_of_dish}</h4>
+        dihesContainerRef.innerHTML += `<div id="dish_container_${index}" class="dish_container" onclick="addToBasket(event)">
+        <h4 id="label_dish_container_${index}">${restaurant.dishes[index].name_of_dish}</h4>
         <span class="description_of_dish">${restaurant.dishes[index].ingredients}</span>
-        <span class="price">${restaurant.dishes[index].price}€</span>
+        <span  id="price_dish_container_${index}" class="price">${restaurant.dishes[index].price}€</span>
         <svg class="plus_symbol" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
             <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 
             0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
           </svg>
           
-</div>`
-
+</div>`;
     }
 }
 
 function dessertsRender() {
     let dihesContainerRef = document.getElementById('desserts_container')
     for (let index = 0; index < restaurant.desserts.length; index++) {
-        dihesContainerRef.innerHTML += `<div id="dish_container_${index}" class="dish_container">
-    <h4>${restaurant.desserts[index].name_of_dessert}</h4>
+        dihesContainerRef.innerHTML += `<div id="dessert_container_${index}" class="dish_container" onclick="addToBasket(event)">
+    <h4 id="label_dessert_container_${index}">${restaurant.desserts[index].name_of_dessert}</h4>
     <span class="description_of_dish">${restaurant.desserts[index].ingredients}</span>
-    <span class="price">${restaurant.desserts[index].price}€</span>
+    <span  id="price_dessert_container_${index}" class="price">${restaurant.desserts[index].price}€</span>
     <svg class="plus_symbol" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
         <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 
         0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
       </svg>
       
-</div>`
+</div>`;
 
     }
 }
 
 function init() {
-    dihesRender()
-    dessertsRender()
+    dihesRender();
+    dessertsRender();
 }
+
+function checkClick(event) {
+    return event.currentTarget.id;
+}
+
+function addToBasket(event) {
+    let idOfElement = checkClick(event);
+    my_Object = createObject(idOfElement);
+    addObjectToBasket(my_Object);
+
+}
+
+function createObject(id) {
+    let my_Object = {
+        'name': document.getElementById(`label_${id}`).innerHTML,
+        'price': parseFloat(document.getElementById(`price_${id}`).innerHTML).toFixed(2),
+        'number': 1
+    }
+    return my_Object;
+}
+
+function addObjectToBasket(my_Object) {
+    if (basket.length == 0) {
+        basket.push(my_Object);
+    }
+    else {
+        indexOfObject = checkObjectPresence(my_Object)
+        if(indexOfObject!=null){
+            basket[indexOfObject].number += 1;
+        }
+        else{
+            basket.push(my_Object);
+        }
+    }
+    console.log(basket);
+}
+
+function checkObjectPresence(my_Object){
+    let indexOfObject = null;
+    for (let index = 0; index < basket.length; index++) {
+        let objectOfBasket = basket[index];
+        if (objectOfBasket.name == my_Object.name) {
+            indexOfObject = index;
+            return indexOfObject;
+        }
+    }
+}
+
+
