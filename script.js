946d4c72d1b0eebfe968d2de615180982fb37cf7
Dmitrii Schilling <schiling2000@gmail.com>
2025-09-18 22:19:06 +0200

    git commit -m "Add basket item management functions: increase quantity, decrease quantity, remove item"

--- script.js
+++ script.js
@@ -48,6 +48,10 @@ function checkClick(event) {
     return event.currentTarget.id;
 }
 
+function checkClick2(event) {
+    return event.target.closest('[id]');
+}
+
 function addToBasket(event) {
     let idOfElement = checkClick(event);
     my_Object = createObject(idOfElement);
@@ -70,17 +74,17 @@ function addObjectToBasket(my_Object) {
     }
     else {
         indexOfObject = checkObjectPresence(my_Object)
-        if(indexOfObject!=null){
+        if (indexOfObject != null) {
             basket[indexOfObject].number += 1;
         }
-        else{
+        else {
             basket.push(my_Object);
         }
     }
-    console.log(basket);
+    renderBasket();
 }
 
-function checkObjectPresence(my_Object){
+function checkObjectPresence(my_Object) {
     let indexOfObject = null;
     for (let index = 0; index < basket.length; index++) {
         let objectOfBasket = basket[index];
@@ -90,5 +94,94 @@ function checkObjectPresence(my_Object){
         }
     }
 }
+
+function renderBasket() {
+    let basketMainRef = document.getElementById('basket_main');
+    basketMainRef.innerHTML = '';
+    for (let index = 0; index < basket.length; index++) {
+        basketMainRef.innerHTML += getBasketHtml(basket[index]);
+    }
+}
+
+function getBasketHtml(my_Object) {
+    return ` <div id="${my_Object.name.replace(/ /g, "_").toLowerCase()}">
+        <h5>${my_Object.name}</h5>
+        <div class="basket_symbols_container">
+            <svg class="basket_symbols" onclick="decreaseBasketItemQuantity(event)" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+            </svg>
+            <span class="number">${my_Object.number}x</span>
+            <svg class="basket_symbols basket_symbols_plus" onclick="increaseBasketItemQuantity(event)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+                stroke="currentColor" stroke-width="1.5" class="size-6" fill="currentColor">
+                <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 
+        0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+            </svg>
+            <span class="basket_price">${parseFloat(my_Object.number * my_Object.price).toFixed(2)}€</span>
+            <svg class="basket_symbols" xmlns="http://www.w3.org/2000/svg" onclick="removeFromBasket(event)" fill="none"
+                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244
+                     2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0
+                      0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09
+                       2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>
+
+
+        </div>
+        
+
+    </div>`
+
+}
+
+function increaseBasketItemQuantity(event) {
+    let containerOfDish = checkClick2(event);
+    let nameOfDish = containerOfDish.querySelector('h5').textContent;
+    for (let index = 0; index < basket.length; index++) {
+        let objectOfBasket = basket[index];
+        if (objectOfBasket.name == nameOfDish) {
+            objectOfBasket.number += 1;
+            break;
+        }
+    }
+    renderBasket();
+}
+
+function decreaseBasketItemQuantity(event) {
+    let containerOfDish = checkClick2(event);
+    let nameOfDish = containerOfDish.querySelector('h5').textContent;
+    for (let index = 0; index < basket.length; index++) {
+        let objectOfBasket = basket[index];
+        if (objectOfBasket.name == nameOfDish) {
+            if(objectOfBasket.number == 1){
+                basket.splice(index, 1);
+                break;
+            }
+            else{
+            objectOfBasket.number -= 1;
+            break;
+            }
+        }
+    }
+    renderBasket();
+}
+
+function removeFromBasket(event) {
+    let containerOfDish = checkClick2(event);
+    let nameOfDish = containerOfDish.querySelector('h5').textContent;
+    for (let index = 0; index < basket.length; index++) {
+        let objectOfBasket = basket[index];
+        if (objectOfBasket.name == nameOfDish) {
+            basket.splice(index, 1);
+            break;
+        }
+    }
+    renderBasket();
+}
+
+
+
+
 
 
